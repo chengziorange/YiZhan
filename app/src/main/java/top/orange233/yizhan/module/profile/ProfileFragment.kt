0430e0958b2158b94766af3658ea0c96886e5403
Orange233 <chengziorange@163.com>
2020-06-23 16:37:28 +0800

    [feat]添加个人资料界面的默认头像

--- app/src/main/java/top/orange233/yizhan/module/profile/ProfileFragment.kt
+++ app/src/main/java/top/orange233/yizhan/module/profile/ProfileFragment.kt
@@ -1,17 +1,26 @@
 package top.orange233.yizhan.module.profile
 
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.gyf.immersionbar.ktx.immersionBar
+import kotlinx.android.synthetic.main.fragment_profile.*
 import top.orange233.yizhan.R
 import top.orange233.yizhan.module.base.BaseFragment
 
-class ProfileFragment:BaseFragment() {
+class ProfileFragment : BaseFragment() {
     override fun getLayout(): Int = R.layout.fragment_profile
 
     override fun initView() {
         immersionBar {
             statusBarColor(R.color.colorPrimary)
-            statusBarDarkFont(false )
+            statusBarDarkFont(false)
         }
+
+        Glide.with(this)
+            .load("https://i.loli.net/2020/06/20/jFJ9YgqMGyfRbNH.png")
+            .apply(RequestOptions.bitmapTransform(CircleCrop()))
+            .into(profile_avator)
     }
 
     override fun initEvent() {
